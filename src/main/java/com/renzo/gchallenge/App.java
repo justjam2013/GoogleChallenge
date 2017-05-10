c1e0f9d660eb2e4811fcac611cf74be9e6e47458
Just Jam <renzo@renzo-zanellis-macbook.local>
2017-05-10 21:50:21 +0100

    Initial code.

---
+++ src/main/java/com/renzo/gchallenge/App.java
@@ -0,0 +1,13 @@
+package com.renzo.gchallenge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
