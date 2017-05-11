3a3ee2a68f31695c24299e0b4b40d47fac10ca83
Just Jam <renzo@renzo-zanellis-macbook.local>
2017-05-11 01:15:54 +0100

    Created App tests.

--- src/test/java/com/renzo/gchallenge/AppTest.java
+++ src/test/java/com/renzo/gchallenge/AppTest.java
@@ -1,38 +1,49 @@
 package com.renzo.gchallenge;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collection;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.renzo.gchallenge.model.Contact;
+import com.renzo.gchallenge.persistence.FilePersistenceService;
+import com.renzo.gchallenge.persistence.PersistenceException;
+import com.renzo.gchallenge.persistence.PersistenceService;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    private static Collection<Contact> contacts;
+
+    @BeforeClass
+    public static void init() throws PersistenceException {
+        PersistenceService persistenceService = new FilePersistenceService();
+        contacts = persistenceService.getContacts();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testGetMaleCount() {
+        long count = App.getMaleCount(contacts);
+
+        assertEquals(3, count);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testGetOldestContact() {
+        Contact contact = App.getOldestContact(contacts);
+
+        assertEquals("Wes", contact.getFirstName());
+        assertEquals("Jackson", contact.getLastName());
+    }
+
+    @Test
+    public void testGetDaysBetweenDob() {
+        long days = App.getDaysBetweenDob(contacts, "Bill", "Paul");
+
+        assertEquals(2862, days);
     }
 }
